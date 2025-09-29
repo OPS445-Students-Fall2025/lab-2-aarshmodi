569a29f78c98a0636d7520015e663364703887e9
aarsh <asmodi2@myseneca.ca>
2025-09-29 12:58:09 -0400

    Added authorship declaration

---
+++ assignment2/duim.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+duim.py - Disk Usage Improved
+
+Description: 
+
+Author: asmodi2
+Date: September 29, 2025
+'''
+
+import sys
+import subprocess
+import argparse
+
+
+def call_du_sub(target_directory):
+    """
+    TODO: Complete this function
+    """
+    pass
+
+
+def percent_to_graph(percent, total_chars):
+    """
+    TODO: Complete this function
+    """
+    pass
+
+
+def create_dir_dict(du_output_list):
+    """
+    TODO: Complete this function
+    """
+    pass
+
+
+def parse_command_args():
+    """
+    TODO: Complete this function
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
